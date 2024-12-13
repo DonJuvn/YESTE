9f419d526097b72ee0006831e9f6f0fe4f670ab4
DonJuan <armvnov1ch@gmail.com>
2024-12-13 17:54:15 +0500

    video with adaptive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Menu from "./components/menu";
+import Video from "./components/video";
 
 function App() {
    return (
       <>
          <Menu />
+         <Video />
       </>
    );
 }
---
+++ src/components/video.jsx
@@ -0,0 +1,16 @@
+import React from "react";const Video = () => {
+   return (
+      <div id="video">
+         <div className="content">
+            <img src="img/logo.svg" alt="" />
+         </div>
+         <video autoPlay loop muted playsInline className="background-video">
+            <source src="img/video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+         </video>
+         <div className="filter"></div>
+      </div>
+   );
+};
+
+export default Video;
