c17dbc319b4122989ee1a9e865fc3a40ca054486
DonJuan <armvnov1ch@gmail.com>
2024-12-13 22:41:55 +0500

    logos loop

--- src/components/logos.jsx
+++ src/components/logos.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 const logosData = [
    { id: 1, src: "img/logo1.svg" },
    { id: 2, src: "img/logo2.svg" },
@@ -11,6 +11,17 @@ const logosData = [
    { id: 10, src: "img/logo10.svg" },
    { id: 11, src: "img/logo11.svg" },
    { id: 12, src: "img/logo12.svg" },
+   { id: 13, src: "img/logo1.svg" },
+   { id: 14, src: "img/logo2.svg" },
+   { id: 15, src: "img/logo3.svg" },
+   { id: 16, src: "img/logo4.svg" },
+   { id: 17, src: "img/logo5.svg" },
+   { id: 18, src: "img/logo6.svg" },
+   { id: 19, src: "img/logo8.svg" },
+   { id: 20, src: "img/logo9.svg" },
+   { id: 21, src: "img/logo10.svg" },
+   { id: 22, src: "img/logo11.svg" },
+   { id: 23, src: "img/logo12.svg" },
 ];
 
 const Logos = () => {
