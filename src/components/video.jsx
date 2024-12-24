2cf06c7e01a21658ae30c1eaa6b6c4cf5725a3e3
DonJuan <armvnov1ch@gmail.com>
2024-12-24 15:35:41 +0500

    test 45

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-const Video = () => {
+import React from "react";const Video = () => {
    return (
       <div id="video">
          <div className="content">
             <img src="img/logo.svg" alt="" />
          </div>
          <video autoPlay loop muted playsInline className="background-video">
-            {/* <source src="/img/video-main1.mp4" type="video/mp4" /> */}
-            <source src="/img/main.mp4" type="video/mp4" />
+            <source src="/img/video45.mp4" type="video/mp4" />
+            {/* <source src="/img/main.mp4" type="video/mp4" /> */}
             Your browser does not support the video tag.
          </video>
          <div className="filter"></div>
